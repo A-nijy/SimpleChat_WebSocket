ea30ac14303caf6cb85df6057c206a31a78d2e75
A-nijy <rainick98@gmail.com>
2024-11-25 21:44:00 +0900

    FIRST COMMIT

---
+++ src/main/java/com/example/SimpleChat_WebSocket/SimpleChatWebSocketApplication.java
@@ -0,0 +1,13 @@
+package com.example.SimpleChat_WebSocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleChatWebSocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleChatWebSocketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SimpleChat_WebSocket/SimpleChatWebSocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SimpleChat_WebSocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleChatWebSocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
